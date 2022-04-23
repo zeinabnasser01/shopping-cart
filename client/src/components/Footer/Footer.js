7f7441cc38a0a5213c5c84d4b4ccaef575f34b6d
Zeinab Nasser <zeinabnasser01@gmail.com>
2022-04-23 04:50:26 +0200

    add data json and wordsjs

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import '../../css/Footer/Footer.css';
+import { words } from '../../words.js';
 
 function Footer() {
-  return <footer>footer</footer>;
+  return <footer>{words.footerTitle}</footer>;
 }
 
 export default Footer;
---
+++ client/src/words.js
@@ -0,0 +1,5 @@
+export const words = {
+  headerTitle: 'Shopping Cart',
+  footerTitle: 'Copyright 2022',
+  content: 'content',
+};
